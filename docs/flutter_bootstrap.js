be2bcf824c4a97169ad9fbc63dc11c2b15f72f81
kashf99 <71818941+kashf99@users.noreply.github.com>
2024-12-12 08:44:17 +0400

    update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "794632776"
+    serviceWorkerVersion: "1981358095"
   }
 });
